00c82edd4547cc97b71354a1020f2d14ac9b2cfa
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-03 15:22:45 +0800

    add: doc

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,15 @@ module.exports = {
     chainWebpack: config => {
         config.plugin('ignore')
             .use(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));  //忽略/moment/locale下的所有文件
+        config.module.rule('md')
+            .test(/\.md/)
+            .use('vue-loader')
+            .loader('vue-loader')
+            .end()
+            .use('vue-markdown-loader')
+            .loader('vue-markdown-loader/lib/markdown-compiler')
+            .options({
+              raw: true
+            })
     }
 }
